0bdfcc258080f4e83c72903cc6cced851b4e283d
Ratana <rathanachan55@gmail.com>
2024-07-06 01:06:53 +0700

    again facebook chkout

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -147,22 +147,18 @@ function MovieDetails() {
     return (
         <div className="Moviedetail-container">
                     <Helmet>
-                    <meta property="og:url" content={window.location.href} />
-                    <meta property="og:type" content="website" />
-                    <meta property="og:title" content={`${movie.name} - Movie Rating`} />
-                    <meta property="og:description" content={`${movie.summary.substring(0, 200)}... Read more and rate this movie!`} />
-                    <meta property="og:image" content={fullImageUrl} />
-                    <meta property="og:image:width" content="1200" />
-                    <meta property="og:image:height" content="630" />
-                    <meta property="og:image:type" content="image/jpeg" />
-                    <meta property="og:image:alt" content={`${movie.name} poster`} />
-                    <meta property="fb:app_id" content="YOUR_FACEBOOK_APP_ID" />
-                    <meta property="og:site_name" content="Your Site Name" />
-                    <meta property="og:rating" content={averageRating.toFixed(1)} />
-                    <meta property="og:rating:scale" content="10" />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:image" content={fullImageUrl} />
-                </Helmet>
+            <meta property="og:url" content={window.location.href} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={`${movie.name} - Movie Rating`} />
+            <meta property="og:description" content={`${movie.summary.substring(0, 200)}... Read more and rate this movie!`} />
+            <meta property="og:image" content={movie.image_poster} />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
+            <meta property="fb:app_id" content="YOUR_FACEBOOK_APP_ID" />
+            <meta property="og:site_name" content="Your Site Name" />
+            <meta property="og:rating" content={averageRating.toFixed(1)} />
+            <meta property="og:rating:scale" content="10" />
+        </Helmet>
             <div className="detailtop-container">
                 <h1>{movie.name}</h1>
                 <div className="Detailtop">
